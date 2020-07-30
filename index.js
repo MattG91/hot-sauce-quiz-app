573e0a3ab7840e2c375a8e5fc98e1ed6fcfec5cb
MattG91 <48453914+MattG91@users.noreply.github.com>
2020-07-30 17:45:51 -0600

    Initial wire frame upload

---
+++ index.js
@@ -0,0 +1,114 @@
+// questions for the quiz
+const STORE = [
+  {
+    question: 'When was the first official American hot sauce produced?',
+    answers: [
+      '1976',
+      '1807',
+      '1853',
+      '1602'
+    ],
+    correctAnswer:
+     '1807'
+  },
+  {
+    question: 'What is the current hottest pepper in the world?',
+    answers: [
+      'Ghost pepper',
+      'Scorpion pepper',
+      'Habeneros',
+      'Carolina Reaper'
+    ],
+    correctAnswer:
+      'Carolina Reaper'
+  },
+  {
+    question: 'What is capsaicin?',
+    answers: [
+      'The oil in hot peppers that causes the feeling of heat',
+      'How hot the peppers look while they are growing',
+      'The amount of nutrients needed to grow the pepper',
+      'how much the pepper seeds can yeild on re-groth'
+    ],
+    correctAnswer: 
+      'The oil in hot peppers that causes the feeling of heat'
+  },
+  {
+    question: 'What was the Bhut Jolokia pepper used for?',
+    answers: [
+      'Home defense',
+      'An extra kick for meals',
+      'Keeping elephants from eating your crops',
+      'Keeping bugs off of your crops'
+    ],
+    correctAnswer: 
+      'Keeping elephants from eating your crops'
+  },
+  {
+    question: 'What does SHU stand for?',
+    answers: [
+      'Super hot unit',
+      'Skeptic hedron uline',
+      'Sensational helix under',
+      'Scoville heat unit'
+    ],
+    correctAnswer: 
+      'Scoville heat unit'
+  },
+  {
+    question: 'What is the 60 second record for most reaper peppers eatin?',
+    answers: [
+      '7',
+      '107 grams',
+      '120 grams',
+      '15'
+    ],
+    correctAnswer:
+      '120 grams'
+  },
+  {
+    question: 'Who invented the Carolina Reaper pepper?',
+    answers: [
+      'Grace Hernandez',
+      'John Knots',
+      'Jocelyn Edwards',
+      'Ed Currie'
+    ],
+    correctAnswer: 
+      'Ed Currie'
+  },
+  {
+    question: 'What is an heirloom pepper seed?',
+    answers: [
+      'A seed that has been mass produced and is stable',
+      'A stable, open breed that has been going for generations',
+      'A very old pepper seed',
+      'A weird magic seed your Grandpa gave to you'
+    ],
+    correctAnswer: 
+      'A stable, open breed that has been going for generations'
+  },
+  {
+    question: 'How tall can a scorpion pepper plant grow?',
+    answers: [
+      '2 feet',
+      '6 feet',
+      '1.5 feet',
+      '4 feet'
+    ],
+    correctAnswer: 
+      '4 feet'
+  },
+  {
+    question: 'What is one benifit eating hot peppers offer?',
+    answers: [
+      'Sweating toxins out of your body',
+      'The oil in the peppers helps your skin health',
+      'Decreasing inflammation of your cells',
+      'Getting sufficient Zink into your diet'
+
+    ],
+    correctAnswer: 
+    'Decreasing inflammation of your cells'
+  }
+]
