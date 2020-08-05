892cf1a9f3e5126554e706b5ad7cc96b0aaddee1
MattG91 <48453914+MattG91@users.noreply.github.com>
2020-08-05 16:53:44 -0600

    latest update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
+/*************************************************Global variables*/ 
 let currentQuestion = 0;
 let correctAnswers = 0;
 let correctAnswerSelector = 0;
 
-
+/*************************************************Event handlers*/ 
 function startQuiz() {
   $('#start-btn').on('click', function(event) {
     $('#start-btn').hide();
@@ -15,51 +17,18 @@ function startQuiz() {
   });
 }
 
-function renderQuestion() {
-  if(currentQuestion < STORE.length) {
-    $('#questionArea').html(`
-      <div class="top-and-bottom-padding">
-        <p class="indie-flower">${STORE[currentQuestion].question}</p>
-        <form class="indie-flower">
-          <fieldset>
-            <legend>
-              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[0]}">
-                <label for="quest1">${STORE[currentQuestion].answers[0]}</label><br>
-              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[1]}">
-                <label for="quest2">${STORE[currentQuestion].answers[1]}</label><br>
-              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[2]}">
-                <label for="quest3">${STORE[currentQuestion].answers[2]}</label><br>
-              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[3]}">
-                <label for="quest4">${STORE[currentQuestion].answers[3]}</label><br>
-            </legend>
-          </fieldset>
-        </form>
-        <p class="indie-flower" id="questionResult"></p>
-      </div>
-    `)
-  }
-}
-
 function submitQuestion() {
   $('#submit-btn').on('click', event => {
-    checkQuestion();
-    increaseAnswerSelector();
-    $('#submit-btn').hide();
-    $('#next-btn').show();
-  })
-}
-
-function checkQuestion() {
-  let selected = $('input:checked');
-  let userAnswer = selected.val();
-  let correctAnswer = STORE[correctAnswerSelector].correctAnswer;
-  if (userAnswer === correctAnswer) {
-    $('#questionResult').text('You got it correct!')
-    increaseCorrectAnswers();
-  }
-  else {
-    $('#questionResult').text(`Almost! the correct answer is "${correctAnswer}"`);
-  }
+    let selected = $('input:checked');
+    let userAnswer = selected.val();
+    if (userAnswer) {
+      checkQuestion(userAnswer);
+      $('#submit-btn').hide();
+      $('#next-btn').show();
+    } else {
+      alert('Please select an answer');
+    }
+  });
 }
 
 function nextQuestion() {
@@ -74,7 +43,61 @@ function nextQuestion() {
       console.log(correctAnswers);
       finalResults();
     }
-  })
+  });
+}
+
+function restartQuiz() {
+  $('#restart-btn').on('click', event => {
+    resetValues();
+    $('.item').show();
+    $('#finalResultsArea').hide();
+    $('#questionArea').show();
+    $('#restart-btn').hide();
+    $('#submit-btn').show();
+    renderQuestion();
+    increaseCurrentQuestion();
+  });
+}
+
+/*************************************************Question generator*/
+function renderQuestion() {
+  if(currentQuestion < STORE.length) {
+    $('#questionArea').html(`
+      <div class="top-and-bottom-padding">
+        <p class="indie-flower">${STORE[currentQuestion].question}</p>
+        <hr>
+        <form class="indie-flower">
+          <fieldset class="margin-top">
+            <legend>
+              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[0]}" required>
+                <label class="questionText li-padding" for="quest1">${STORE[currentQuestion].answers[0]}</label><br>
+              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[1]}">
+                <label class="questionText li-padding" for="quest2">${STORE[currentQuestion].answers[1]}</label><br>
+              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[2]}">
+                <label class="questionText li-padding" for="quest3">${STORE[currentQuestion].answers[2]}</label><br>
+              <input type="radio" name="possibleAnswers" value="${STORE[currentQuestion].answers[3]}">
+                <label class="questionText li-padding" for="quest4">${STORE[currentQuestion].answers[3]}</label><br>
+            </legend>
+          </fieldset>
+        </form>
+        <p class="indie-flower" id="questionResult"></p>
+      </div>
+    `)
+  }
+}
+
+/*************************************************Single purpose functions*/ 
+function checkQuestion(userAnswer) {
+  
+  let correctAnswer = STORE[correctAnswerSelector].correctAnswer;
+  if (userAnswer === correctAnswer) {
+    $('#questionResult').text('You got it correct!')
+    increaseCorrectAnswers();
+  }
+  else {
+    $('#questionResult').text(`Almost! the correct answer is "${correctAnswer}"`);
+  }
+  increaseAnswerSelector();
 }
 
 function finalResults() {
@@ -85,7 +108,7 @@ function finalResults() {
   $('#finalResultsHeader').text('Here are your test results');
   let greatScore = `Great job, you got a perfect score! ${correctAnswers} out of 10!`;
   let okScore = `Not bad! You got ${correctAnswers} out of 10. Want to try again?`;
-  let badScore = `Looks like you could use some practice! You got ${correctAnswers} out of 10. Try again.`;
+  let badScore = `Looks like you could use some practice! You got ${correctAnswers} out of 10. Try again!`;
   if (correctAnswers === 10) {
     $('#finalResults').text(greatScore);
   }
@@ -98,22 +121,14 @@ function finalResults() {
   $('#finalResultsArea').show();
 }
 
-function restartQuiz() {
-  $('#restart-btn').on('click', event => {
-    currentQuestion = 0;
-    correctAnswers = 0;
-    correctAnswerSelector = 0;
-    $('.item').show();
-    $('.currentQuestion').text(1);
-    $('.currentScore').text(0);
-    $('#finalResultsArea').hide();
-    $('#questionArea').show();
-    $('#restart-btn').hide();
-    $('#submit-btn').show();
-    renderQuestion();
-    increaseCurrentQuestion();
-  });
+function resetValues() {
+  currentQuestion = 0;
+  correctAnswers = 0;
+  correctAnswerSelector = 0;
+  $('.currentQuestion').text(1);
+  $('.currentScore').text(0);
 }
+
 
 function increaseCurrentQuestion() {
   currentQuestion++;
@@ -129,12 +144,13 @@ function increaseAnswerSelector()  {
   correctAnswerSelector++;
 }
 
+/*************************************************Initializing function*/ 
 function makeQuiz() {
   startQuiz();
   submitQuestion();
-  checkQuestion();
   nextQuestion();
   restartQuiz();
 }
 
 $(makeQuiz);
+
